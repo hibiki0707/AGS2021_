d3af09496e6206466a9436965579053649066fdf
HibikiAraki <moon707.piano@icloud.com>
2021-05-13 17:50:06 +0900

    追加

--- Skelton/Skelton/maincpp.cpp
+++ Skelton/Skelton/maincpp.cpp
@@ -3,11 +3,13 @@
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ChangeWindowMode(true);
 	DxLib_Init();
+
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	while (ProcessMessage() != -1) {
 		ClearDrawScreen();
-		//DrawString(100, 100, ,0x000000);
-		DxLib_End();
+		DrawString(100, 100, L"Hello World", 0x000000);
+		ScreenFlip();
 	}
+	DxLib_End();
 }
